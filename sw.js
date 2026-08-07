0b06d5688977609fdd3b6dc4543dfa79aa9e08c8
drrajeshsao-cpu <drrajeshsao@gmail.com>
2026-08-07 17:27:41 +0530

    Add SKMCIS V4.4 clinical prescriber foundation with color-coded examination investigations treatment follow-up

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='skmcis-study-v4-3-1-reader-fix-20260807';
+const C='skmcis-study-v4-4-clinical-prescriber-20260807';
 const SHELL=["./","./index.html","./styles.css?v=4.3.1","./app.js?v=4.3.1","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
